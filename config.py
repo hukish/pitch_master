5d5f34e8cb61775b69b84ff93e6ae3c79b13376c
hukish <hudsonhukish@gmail.com>
2020-05-06 13:55:14 +0300

    modify changes

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+
+class Config:
+
+    
+    
+    SECRET_KEY =('1234')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hudson:1234@localhost/pet'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='1234'
+export MAIL_USERNAME=hudsonhukish@gmail.com
+export MAIL_PASSWORD=1234kishoyian
+
+python3.6 manage.py server
